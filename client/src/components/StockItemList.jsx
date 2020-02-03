47bbb6465d3d2c051f190e221eb9d31e3a4c3b0d
Nick Morgan <Nicholas.Morgan@ibm.com@Nicks-MacBook-Pro-3.local>
2020-02-03 16:03:15 +0000

    Updates the StockItemsList view

--- client/src/components/StockItemList.jsx
+++ client/src/components/StockItemList.jsx
@@ -13,44 +13,45 @@ import { iconCheckmarkSolid } from "carbon-icons";
 import Header from "../pattern-components/Header";
 import "../pattern-components/patterns.scss";
 
-class TableList extends Component {
-  title = 'Table List';
-  subtitle = 'This pattern will display and array of model objects in a multi column grid/table.';
-
-  columns = ['Name', 'Address', 'City', 'State', 'ZipCode', 'Country'];
-  formatters = {
-    'ZipCode': function(val) {
-      return val + '-0000';
-    }
-  };
-
-  data = [
-    {
-      Name: "Lin",
-      Address: "123 Main Street",
-      City: "Austin",
-      State: "TX",
-      ZipCode: "12345",
-      Country: "United States"
-    },
-    {
-      Name: "Mak",
-      Address: "45 2nd Street",
-      City: "Austin",
-      State: "TX",
-      ZipCode: "78766",
-      Country: "United States"
-    },
-    {
-      Name: "Joe",
-      Address: "40 Down Street",
-      City: "San Francisco",
-      State: "CA",
-      ZipCode: "90706",
-      Country: "United States"
-    }
-  ];
-
+class StockItemList extends Component {
+    title = 'Stock Items';
+    subtitle = 'This is the current inventory of items';
+    columns = [
+      "name",
+      "description",
+      "stock",
+      "unitPrice",
+      "picture",
+      "manufacturer",
+    ];
+    formatters = {};
+    data = [
+      {
+        "name": "Item 1",
+        "description": "The first item",
+        "stock": 10,
+        "unitPrice": 100.0,
+        "picture": "test",
+        "manufacturer": "unknown",
+      },
+      {
+        "name": "Item 2",
+        "description": "The second item",
+        "stock": 15,
+        "unitPrice": 120.5,
+        "picture": "test1",
+        "manufacturer": "Apple",
+      },
+      {
+        "name": "Item 3",
+        "description": "The third item",
+        "stock": 20,
+        "unitPrice": 75.5,
+        "picture": "test1",
+        "manufacturer": "Sony",
+      }
+    ];
+    
   constructor(props) {
     super(props);
     this.state = {
@@ -141,4 +142,4 @@ class TableList extends Component {
   }
 }
 
-export default TableList;
+export default StockItemList;
